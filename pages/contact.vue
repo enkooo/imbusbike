c2b4337a991b689a2bb4065f1de8c45116b49b8d
enkooo <pytlos1337@gmail.com>
2024-11-24 20:52:42 +0100

    fix: adjust padding

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div class="min-h-[calc(100dvh-189px)] bg-white shadow-2xl">
-      <ContactSection class="pt-16" />
+      <ContactSection class="pb-24 pt-16" />
     </div>
   </main>
 </template>
--- pages/offer.vue
+++ pages/offer.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div class="min-h-[calc(100dvh-189px)] bg-white shadow-2xl">
-      <OfferSection class="py-24 pt-16" />
+      <OfferSection class="pb-24 pt-16" />
     </div>
   </main>
 </template>
